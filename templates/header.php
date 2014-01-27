24370e88c5fab6f2b1c685bc8c5c7c2c3ccd4bd3
Douglas Gardner <douglas@chippy.ch>
2014-01-27 14:20:39 +0000

    Add encoding metatag to header file

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Hello</title>
+    <meta charset="utf-8">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/bootswatch/3.0.2/yeti/bootstrap.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
