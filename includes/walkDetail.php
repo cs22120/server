3dd9081c37bf82442f0826928c5df14d6fa8b27b
Douglas Gardner <douglas@chippy.ch>
2014-01-27 12:52:09 +0000

    improve walk detail
    Walk detail now shows a list of names of waypoints.

--- includes/walkDetail.php
+++ includes/walkDetail.php
@@ -13,23 +13,33 @@ require_once 'includes/database.php';
 require_once 'includes/templates.php';
 $id = $_GET['walk'];
 
-if ( !is_numeric($id) ) {
-  header('HTTP/1.1 400 Bad Request');
-  render('message', ['Error 400: Bad walk ID', 'The walk ID you requested is invalid.']);
+if ( !is_numeric( $id ) ) {
+  header( 'HTTP/1.1 400 Bad Request' );
+  render( 'message', ['Error 400: Bad walk ID', 'The walk ID you requested is invalid.'] );
   die();
 }
 
-$query = executeSql("SELECT * FROM tbl_routes WHERE (id = $id)");
-if ( is_object($query) === FALSE ) {
-  render('message',['Database error', 'We were unable to get the details of your walk.', $query]);
+$query = executeSql( "SELECT * FROM tbl_routes, tbl_places WHERE (tbl_routes.id = $id)" );
+if ( is_object( $query ) === FALSE ) {
+  render( 'message', ['Database error', 'We were unable to get the details of your walk.', $query] );
   die();
 }
 if ( $query->num_rows == 0 ) {
-  header('HTTP/1.1 404 File Not Found');
-  render('message', ['Error 404: Walk not found', 'Please check the walk ID and try again.']);
+  header( 'HTTP/1.1 404 File Not Found' );
+  render( 'message', ['Error 404: Walk not found', 'Please check the walk ID and try again.'] );
   die();
 }
-$result = $query->fetch_assoc();
-render('walkDetail',$result);
+
+while ( $walk = $query->fetch_assoc() ) {
+  if ( !isset( $body ) ) {
+    $body = "";
+    $body .= '<h1>' . $walk['title'] . '</h1>';
+    $body .= '<p class="lead">' . $walk['longDesc'] . '</p><ol>';
+  }
+  $body .= '<li>' . $walk['description'] . '</li>';
+}
+$body .= '</ol>';
+
+render( 'skeleton', $body );
 
 ?>
---
+++ templates/skeleton.php
@@ -0,0 +1,3 @@
+<?php require_once( 'templates/header.php' );
+echo $data;
+require_once( 'templates/footer.php' ); ?>
