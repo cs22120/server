54255025ebb3bcf35d8b5eda71bdc2fe0b68f030
Douglas Gardner <douglas@chippy.ch>
2014-01-27 11:59:15 +0000

    Start walk detail page

--- includes/walkDetail.php
+++ includes/walkDetail.php
@@ -9,8 +9,27 @@
 if ( !defined( 'ENTRYPOINT' ) ) {
   die( 'Not a valid entry point.' );
 }
+require_once 'includes/database.php';
+require_once 'includes/templates.php';
+$id = $_GET['walk'];
 
-require 'includes/templates.php';
-header( 'HTTP/1.1 501 Not Implemented' );
-render( 'message', ['Walk details', 'This functionality has not been implemented yet.'] );
+if ( !is_numeric($id) ) {
+  header('HTTP/1.1 400 Bad Request');
+  render('message', ['Error 400: Bad walk ID', 'The walk ID you requested is invalid.']);
+  die();
+}
+
+$query = executeSql("SELECT * FROM tbl_routes WHERE (id = $id)");
+if ( is_object($query) === FALSE ) {
+  render('message',['Database error', 'We were unable to get the details of your walk.', $query]);
+  die();
+}
+if ( $query->num_rows == 0 ) {
+  header('HTTP/1.1 404 File Not Found');
+  render('message', ['Error 404: Walk not found', 'Please check the walk ID and try again.']);
+  die();
+}
+$result = $query->fetch_assoc();
+render('walkDetail',$result);
+
 ?>
---
+++ templates/walkDetail.php
@@ -0,0 +1,4 @@
+<?php require_once( 'templates/header.php' ); ?>
+<h1><?php echo $data['title']; ?></h1>
+<p class="lead"><?php echo $data['longDesc']; ?></p>
+<?php require_once( 'templates/footer.php' ); ?>
