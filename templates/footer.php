19dfd4ce3f5bb30619c96edc5fcb5803abde2d2c
Douglas Gardner <douglas@chippy.ch>
2014-01-30 11:46:10 +0000

    fix rel link?

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
     <div class="container">
       <p class="text-muted">
-        &copy; 2014 <a href="/?credits">contributors</a></p>
+        &copy; 2014 <a href="?credits">contributors</a></p>
       </p>
     </div>
 </div>
